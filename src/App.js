63c286ca2437514f57c4f30182ff22d3ab7ff8af
michal.dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-02-05 00:14:14 +0100

    creating components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person name="Mike" age="28" />
+        <Person name="John" age="15">My hobbies: </Person>
+        <Person name="Lisa" age="62" />
       </div>
     );
   }
