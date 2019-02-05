078cf0dba0788d623dca0063a670ec8cf42b550c
michal.dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-02-05 22:08:57 +0100

    component event handling and state basics

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 8 },
+        { name: newName, age: 9 },
+        { name: newName, age: 6 }
+      ],
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Mike" age="28" />
-        <Person name="John" age="15">My hobbies: </Person>
-        <Person name="Lisa" age="62" />
+        <button onClick={() => this.switchNameHandler('FuncMessi')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Messi 2')} >My hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
   }
